69b003804827df8342b5ff4fed91758fe9e815d2
Caleb Wang <156046197+cayleb247@users.noreply.github.com>
2025-09-25 23:33:55 -0400

    Change auth default
    Landing is default again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   /* For testing im just going to test dashboard as the default, idk auth lol*/
   return (
     <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 flex justify-center align-center box-border pt-10">
-      {(await authenticated()) ? <Landing /> : <Dashboard /> }
+      {(await authenticated()) ? <Dashboard /> : <Landing /> }
     </div>
   );
 }
